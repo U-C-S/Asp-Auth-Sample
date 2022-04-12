549238830b4831f3a0683cdded8c47e90730e4a4
U-C-S <ucs3721@outlook.com>
2022-04-12 18:35:01 +0530

    Add User Model and other tweaks

---
+++ Models/UserDto.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspAuth.Models;
+
+public class UserLogin
+{
+    [Required]
+    public string Username { get; set; }
+
+    [Required]
+    public string Password { get; set; }
+}
+
+public class UserRegistration
+{
+    [Required]
+    public string Username { get; set; }
+
+    [Required]
+    public string Password { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+}
+
